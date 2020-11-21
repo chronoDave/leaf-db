6ec1ba496ba4902bb046af5d6f80856a9784d64b
chronoDave <davidwolters6758@gmail.com>
2020-11-21 15:00:26 +0100

    Removed dist

--- dist/modifiers.d.ts
+++
@@ -1,3 +0,0 @@
-import { Projection, NewDoc, Update } from './types';
-export declare const objectModify: (object: object, update: NewDoc | Update) => object;
-export declare const objectProject: (object: NewDoc, projection: Projection) => {} | null;
