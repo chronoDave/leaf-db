1c0c30f13be31bd75f6b540f07dc96ae3b2ae6a4
chronoDave <davidwolters6758@gmail.com>
2022-05-06 14:19:35 +0200

    Simplified errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,19 +1,15 @@
-const toString = (x: unknown) => typeof x === 'object' ?
-  JSON.stringify(x) :
-  `${x}`;
-
 export const MEMORY_MODE = (fn: string) => `Tried to call '${fn}()' in memory mode`;
 export const DUPLICATE_DOC = (doc: { _id: string }) => `Duplicate doc: ${doc._id}`;
 
-const INVALID = (param: string, value: unknown) => `Invalid ${param}: ${toString(value)}`;
-export const INVALID_DOC = (doc: unknown) => INVALID('doc', doc);
-export const INVALID_ID = (_id: string) => INVALID('_id', _id);
-export const INVALID_QUERY = (query: unknown) => INVALID('query', query);
-export const INVALID_UPDATE = (update: unknown) => INVALID('update', update);
-export const INVALID_OPERATOR = (operator: unknown) => INVALID('operator', operator);
-export const INVALID_MODIFIER = (modifier: unknown) => INVALID('modifier', modifier);
-export const INVALID_PROJECTION = (projection: unknown) => INVALID('projection', projection);
+const INVALID = (param: string) => (value: unknown) => `Invalid ${param}: ${JSON.stringify(value)}`;
+export const INVALID_DOC = INVALID('doc');
+export const INVALID_ID = INVALID('_id');
+export const INVALID_QUERY = INVALID('query');
+export const INVALID_UPDATE = INVALID('update');
+export const INVALID_OPERATOR = INVALID('operator');
+export const INVALID_MODIFIER = INVALID('modifier');
+export const INVALID_PROJECTION = INVALID('projection');
 
-const NOT = (value: unknown, type: string) => `Invalid value '${toString(value)} (${typeof value})', expected type '${type}'`;
-export const NOT_ARRAY = (value: unknown) => NOT(value, 'Array');
-export const NOT_NUMBER = (value: unknown) => NOT(value, 'Number');
+const NOT = (type: string) => (value: unknown) => `Invalid value '${JSON.stringify(value)} (${typeof value})', expected type '${type}'`;
+export const NOT_ARRAY = NOT('Array');
+export const NOT_NUMBER = NOT('Number');
