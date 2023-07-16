e641ca4e0aecbe9ec55352aa14237c5795b356e7
chronoDave <davidwolters6758@gmail.com>
2023-07-16 13:04:35 +0100

    Fixed tests

--- test/unit/validation/isDraft.spec.ts
+++ test/unit/validation/isDraft.spec.ts
@@ -5,11 +5,8 @@ import { isDraft } from '../../../src/validation';
 
 test('[isDraft] should return false if doc is invalid', t => {
   t.false(isDraft({ $field: 2 }), 'operator');
-  t.false(isDraft({ 'test.field': 2 }), 'dot');
   t.false(isDraft({ a: { $field: 2 } }), 'nested operator');
-  t.false(isDraft({ a: { 'test.field': 2 } }), 'nested dot');
   t.false(isDraft({ a: [{ $field: 2 }] }), 'array nested operator');
-  t.false(isDraft({ a: [{ 'test.field': 2 }] }), 'array nested dot');
 
   t.end();
 });
@@ -24,6 +21,9 @@ test('[isDraft] should return true is doc is valid', t => {
   t.true(isDraft({ a: '反復回転時計' }));
   t.true(isDraft({ a: 'a\\null\\undefined' }));
   t.true(isDraft({ _id: '1', a: { b: [{ c: [undefined] }] } }));
+  t.true(isDraft({ 'test.field': 2 }));
+  t.true(isDraft({ a: { 'test.field': 2 } }));
+  t.true(isDraft({ a: [{ 'test.field': 2 }] }));
 
   t.end();
 });
